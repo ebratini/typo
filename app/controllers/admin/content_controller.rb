52d4818654e52b8eb2d14226e5eb19457bffa10d
Edwin Bratini <edwin.bratini@gmail.com>
2016-02-05 12:06:07 +0000

    Prepare 3 hw delivery

--- app/controllers/admin/content_controller.rb
+++ app/controllers/admin/content_controller.rb
@@ -116,15 +116,29 @@ class Admin::ContentController < Admin::BaseController
   def merge
     if current_user.admin?
       begin
-        t_article = Article.find(params[:id].to_i)
-        @article = t_article.merge_with(params[:merge_with])
+        a = Article.find(params[:id].to_i)
+        ma = a.merge_with(params[:merge_with])
+    
+        a.destroy
         
         flash[:notice] = "Article successfully merged."
-        redirect_to "/admin/content/edit/#{ merged_article.id }"
-      rescue ActiveRecord::RecordNotFound, Article::MergeError => me
-        flash[:error] = me.message
+        redirect_to action: :index
+      rescue => e
+        flash[:error] = e.message
         redirect_to "/admin/content/edit/#{ params[:id] }"
       end
+      
+      
+      # begin
+      #   t_article = Article.find(params[:id].to_i)
+      #   @article = t_article.merge_with(params[:merge_with])
+        
+      #   flash[:notice] = "Article successfully merged."
+      #   redirect_to "/admin/content/edit/#{ merged_article.id }"
+      # rescue ActiveRecord::RecordNotFound, Article::MergeError => me
+      #   flash[:error] = me.message
+      #   redirect_to "/admin/content/edit/#{ params[:id] }"
+      # end
     else
       flash[:error] = 'You\'re not allowed to do that.'
       redirect_to action: :index
--- app/models/article.rb
+++ app/models/article.rb
@@ -420,10 +420,13 @@ class Article < Content
   
   def merge_with(article_id)
     raise Article::MergeError, 'Invalid article id provided.' unless article_id.to_s =~ /^\d+$/
-    raise Article::MergeError, 'Cannot merge itself.' if self.id == article_id.to_i
+    raise Article::MergeError, 'Cannot merge itself.' if self.id == article_id
     begin
+      
       mergee_article = Article.find(article_id.to_i)
-      merged_article = Article.create!(title: self.title, body: "#{ self.body } #{ mergee_article.body }")
+      
+      newbody = self.body + ' ' + mergee_article.body
+      merged_article = Article.create!(title: self.title, body: newbody)
       
       # moving comments
       update_article_reference = ['article_id = ?', merged_article.id]
